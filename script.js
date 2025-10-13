560b5089ce3ace7a4232d02bea7ef34b47d8bf0c
Filipe Christian <147321627+FilipeChristian@users.noreply.github.com>
2025-10-12 21:05:59 -0300

    lastup

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-let count = 1;
-setInterval( function(){
-    document.getElementById("radio1").checked;
+    let count = 1;
+let intervelID = setInterval(function(){
+    count++
+    if(count > 3){
+        count = 1;
+    }
     
+    for(let i = 1;i <= 3;i++){
+        document.getElementById("radio" + i).checked = false
+    }
+
+    document.getElementById("radio" + count).checked = true
 
 }, 2000)
+
+for(let i = 1;i < 3;i++){
+    const radio = document.getElementById("radio" + i)
+
+    radio.addEventListener('click', function(){
+        clearInterval(intervelID);
+    })
+
+
+}
 
